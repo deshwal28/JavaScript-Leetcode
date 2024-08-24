26ef50eae7e89e43da306059614d5ca898a73f77
deshwal28 <deshwalshab65@gmail.com>
2024-08-24 22:37:19 +0530

    commit

---
+++ Day6.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let fil=[]
+    for(let i=0;i<arr.length;i++){
+        if(fn(arr[i],i)){
+            fil.push(arr[i]);
+        }
+    }
+    return fil
+    
+};
