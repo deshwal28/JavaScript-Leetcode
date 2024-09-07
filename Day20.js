fd7c392cb1e99411a9f4219d59ccad469790a4ff
deshwal28 <deshwalshab65@gmail.com>
2024-09-07 23:45:41 +0530

    commit

---
+++ Day20.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    if(Array.isArray(obj)){
+        return obj.length===0
+    }
+    else
+    {
+        let arr = Object.keys(obj)
+        return arr.length===0
+    }
+};
